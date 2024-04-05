87abf4fc5644cd96256ebe77028f215e2a1366f9
6574114 <j.f.wiegner@uu.nl>
2024-04-05 16:41:29 +0200

    Throw error if data is not loaded

--- pages/1_Visualize_Single_Result.py
+++ pages/1_Visualize_Single_Result.py
@@ -13,24 +13,28 @@ st.sidebar.markdown('**Cash Status**')
 show_sidebar()
 st.sidebar.markdown("---")
 
-# Page to show
-st.sidebar.markdown('**Graph**')
-pages_available = ["Technology Design", "Network Design",
-                   "Energy Balance at Node",
-                   "Technology Operation", "Network Operation"]
-selected_page = st.sidebar.selectbox("Select graph", pages_available)
+if st.session_state['Result1']:
+    # Page to show
+    st.sidebar.markdown('**Graph**')
+    pages_available = ["Technology Design", "Network Design",
+                       "Energy Balance at Node",
+                       "Technology Operation", "Network Operation"]
+    selected_page = st.sidebar.selectbox("Select graph", pages_available)
 
-# Individual pages
-if selected_page == "Technology Design":
-    plot_technology_design()
-elif selected_page == "Network Design":
-    plot_network_design()
-elif selected_page == "Energy Balance at Node":
-    plot_energy_balance()
-elif selected_page == "Technology Operation":
-    plot_technology_operation()
-elif selected_page == "Network Operation":
-    plot_network_operation()
+    # Individual pages
+    if selected_page == "Technology Design":
+        plot_technology_design()
+    elif selected_page == "Network Design":
+        plot_network_design()
+    elif selected_page == "Energy Balance at Node":
+        plot_energy_balance()
+    elif selected_page == "Technology Operation":
+        plot_technology_operation()
+    elif selected_page == "Network Operation":
+        plot_network_operation()
+
+else:
+    st.markdown("Please load in data first")
 
 
 
