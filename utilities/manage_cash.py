e6b467bceb7d8fc423e6d4cf4fdd8cc55a0fda1f
Jean <j.f.wiegner@uu.nl>
2024-06-21 17:12:08 +0200

    remove second results

--- utilities/manage_cash.py
+++ utilities/manage_cash.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from .read_data import read_results_from_h5
 
+
 def show_sidebar():
     """
     Shows cash status and button in sidebar
@@ -11,12 +12,13 @@ def show_sidebar():
     clear_cash()
     show_cash_status()
 
+
 def show_cash_status():
     """
     Displays that cash status
     :return:
     """
-    if st.session_state['Result1']:
+    if st.session_state["Result1"]:
         st.sidebar.success("Results 1 successfully loaded")
     else:
         st.sidebar.error("Results 1 not loaded")
@@ -26,20 +28,22 @@ def show_cash_status():
     # else:
     #     st.sidebar.error("Results 2 not loaded")
 
-    if  isinstance(st.session_state['NodeLocations'], pd.DataFrame):
+    if isinstance(st.session_state["NodeLocations"], pd.DataFrame):
         st.sidebar.success("Node locations successfully loaded")
     else:
         st.sidebar.error("Node locations not loaded")
+
 
 def clear_cash():
     """
     Clears cash
     :return:
     """
-    if st.sidebar.button('Reset data'):
-        st.session_state['Result1'] = {}
+    if st.sidebar.button("Reset data"):
+        st.session_state["Result1"] = {}
         # st.session_state['Result2'] = {}
-        st.session_state['NodeLocations'] = None
+        st.session_state["NodeLocations"] = None
+
 
 def load_result_data_in_cash():
     """
@@ -48,11 +52,8 @@ def load_result_data_in_cash():
     """
     uploaded_h5 = st.file_uploader("Load a result h5 file (case 1)")
     if uploaded_h5 is not None:
-        st.session_state['Result1'] = read_results_from_h5(uploaded_h5)
+        st.session_state["Result1"] = read_results_from_h5(uploaded_h5)
 
-    uploaded_h5 = st.file_uploader("Load a result h5 file (case 2)")
-    if uploaded_h5 is not None:
-        st.session_state['Result2'] = read_results_from_h5(uploaded_h5)
 
 def load_node_data_in_cash():
     """
@@ -61,10 +62,14 @@ def load_node_data_in_cash():
     """
     uploaded_csv = st.file_uploader("Load Node Locations")
     if uploaded_csv is not None:
-        st.session_state['NodeLocations'] = pd.read_csv(uploaded_csv, sep=';', index_col=0)
+        st.session_state["NodeLocations"] = pd.read_csv(
+            uploaded_csv, sep=";", index_col=0
+        )
 
-    st.markdown("If you wish to plot networks, enter the node locations as a csv here. You can download a sample CSV"
-                " file by clicking on the button below.")
+    st.markdown(
+        "If you wish to plot networks, enter the node locations as a csv here. You can download a sample CSV"
+        " file by clicking on the button below."
+    )
 
     with open("./utilities/Node_Locations.csv", "rb") as file:
         btn = st.download_button(
@@ -72,5 +77,3 @@ def load_node_data_in_cash():
             data=file,
             file_name="Node_Locations.csv",
         )
-
-
